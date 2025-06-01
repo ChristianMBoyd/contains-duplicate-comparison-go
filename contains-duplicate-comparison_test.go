d1a23404ddf7cc564c06c0d349d13f9c1596ca4c
Christian <Boyd.Christian.M@gmail.com>
2025-05-31 22:19:49 -0500

    Adding a simple test case to TestUsingMap

--- contains-duplicate-comparison_test.go
+++ contains-duplicate-comparison_test.go
@@ -4,8 +4,30 @@ import (
 	"testing"
 )
 
+type containsDuplicateTest struct {
+	nums              []int
+	name              string
+	containsDuplicate bool
+}
+
+func generateContainsDuplicateTests() []containsDuplicateTest {
+	return []containsDuplicateTest{
+		{
+			nums:              []int{0},
+			name:              "single element",
+			containsDuplicate: false,
+		},
+	}
+}
+
 func TestUsingMap(t *testing.T) {
-	// Test that this is, in fact, a valid solution
+	for _, test := range generateContainsDuplicateTests() {
+		t.Run(test.name, func(t *testing.T) {
+			if containsDuplicate := UsingMap(test.nums); containsDuplicate != test.containsDuplicate {
+				t.Errorf("UsingMap returned %v when the correct answer was %v", containsDuplicate, test.containsDuplicate)
+			}
+		})
+	}
 }
 
 func BenchmarkUsingMap(b *testing.B) {
