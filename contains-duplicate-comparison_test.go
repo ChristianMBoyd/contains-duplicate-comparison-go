972ab629b8e3cd5c89ec4c5164c182984ef66955
Christian <Boyd.Christian.M@gmail.com>
2025-05-31 22:02:50 -0500

    Initial test skeleton for UsingMap

---
+++ contains-duplicate-comparison_test.go
@@ -0,0 +1,13 @@
+package containsDuplicate
+
+import (
+	"testing"
+)
+
+func TestUsingMap(t *testing.T) {
+	// Test that this is, in fact, a valid solution
+}
+
+func BenchmarkUsingMap(b *testing.B) {
+	// Check the performance
+}
